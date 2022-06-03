e8666197587963acecebfe0b16347597a8961a62
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-03 12:20:50 +0530

    ADDED: Printed welcome message

---
+++ src/WelcomeMessage.java
@@ -0,0 +1,6 @@
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        //Welcome message printed
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}

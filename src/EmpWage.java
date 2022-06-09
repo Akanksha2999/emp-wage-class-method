75bcad1b8dc53efa64b5c08fb3b25c8b9f59989b
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-09 10:19:13 +0530

    ADDED: Saved total wage of employee of different comapany

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -3,15 +3,29 @@ public class EmpWage {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-    //Method computeEmpWage which checks if the employee is working for full time or part time or is absent and gives working hours accordingly
-    public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxWorkingHours) {
+    private final String company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxWorkingHours;
+    private int totalEmpWage;
+
+    //Objects in EmpWage method
+    public EmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxWorkingHours) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxWorkingHours = maxWorkingHours;
+
+    }
+
+    //Method computeEmpWage which checks if the employee is working for full time or part time or is absent and gives working hours accordingly
+    public void computeEmpWage() {
 
         //Initializing variables
         int totalEmpHours = 0;
         int empHours = 0;
         int totalWorkingDays = 0;
-        int totalEmpWage = 0;
 
         /*Checking if the employee is full time working or part time or is absent using switch case*/
         while (totalEmpHours <= maxWorkingHours && totalWorkingDays < numOfWorkingDays) {
@@ -29,20 +43,32 @@ public class EmpWage {
             }
             totalEmpHours += empHours;
             //Printing total working days and hours
-            System.out.println("DAY#: " + totalWorkingDays + " Emp hour: " + empHours);
+            System.out.println("DAY : " + totalWorkingDays + " Emp hour: " + empHours);
         }
+
         //Calculating total employee wage of particular company employee and then printing it
         totalEmpWage = totalEmpHours * empRatePerHour;
-        System.out.println("Employees's Total Wage for company : " + company + " is " + totalEmpWage);
-        return totalEmpWage;
+    }
+
+    //Returning the company name and employees of that comapny's total wage
+    public String toString() {
+        return ("Total Emp Wage for company: " + company + " is: " + totalEmpWage);
     }
 
     //Main method calling computeEmpWage method
     public static void main(String[] args) {
-        computeEmpWage("Bridgelabz", 20, 4, 20);
-        computeEmpWage("Hike", 10, 4, 20);
-        computeEmpWage("Edu", 30, 6, 60);
-        computeEmpWage("Rising", 10, 8, 40);
+        EmpWage Bridgelabz = new EmpWage("Bridgelabz", 20, 4, 20);
+        EmpWage Hike = new EmpWage("Hike", 10, 4, 20);
+        EmpWage Edu = new EmpWage("Edu", 30, 6, 60);
+        EmpWage Rising = new EmpWage("Rising", 10, 8, 40);
+        Bridgelabz.computeEmpWage();
+        System.out.println(Bridgelabz);
+        Hike.computeEmpWage();
+        System.out.println(Hike);
+        Edu.computeEmpWage();
+        System.out.println(Edu);
+        Rising.computeEmpWage();
+        System.out.println(Rising);
 
     }
 }
